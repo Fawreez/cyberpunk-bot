9050c40079a11871b04f30b46599dcb950712dbc
Fawreez <malfarizi42@gmail.com>
2023-04-05 12:16:17 +0700

    Fix postgres_url not defined

--- src/bot.js
+++ src/bot.js
@@ -10,6 +10,14 @@ const Keyv = require('keyv');
 require('dotenv').config()
 
 // Keyv
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_NAME = process.env.DB_NAME
+
+const postgres_url = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+
 const prefixes = new Keyv(postgres_url, { table: 'prefixes' });
 
 const token = process.env.DISCORD_TOKEN
